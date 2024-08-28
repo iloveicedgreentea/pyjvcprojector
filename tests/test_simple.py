c094768786977de5bf3f0733eb62b13fd5387a3c
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-08-27 20:35:56 -0400

    feat: add functional test

---
+++ tests/test_simple.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+import pytest
+import os
+from jvcprojector.projector import JvcProjector
+from jvcprojector import const
+
+logging.basicConfig(level=logging.WARNING)
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    os.environ.get("RUN_INTEGRATION") != "true", reason="Set RUN_INTEGRATION to true"
+)
+async def test_connect():
+    ip = os.getenv("JVC_IP")
+    password = os.getenv("JVC_PASSWORD")
+    if not ip or not password:
+        print("Set JVC_IP and JVC_PASSWORD environment variables")
+        pytest.fail("Set JVC_IP and JVC_PASSWORD environment variables")
+
+    jp = JvcProjector(ip, password=password)
+    await jp.connect()
+
+    print("Projector info:")
+    print(await jp.get_info())
+
+    if await jp.get_power() != const.ON:
+        await jp.power_on()
+        print("Waiting for projector to warmup...")
+        while await jp.get_power() != const.ON:
+            await asyncio.sleep(3)
+
+    await jp.send_command(const.CMD_PICTURE_MODE_LASER_POWER, const.VAL_LASER_POWER[1])
+    print("Current state:")
+    state = await jp.get_state()
+    for key, value in state.items():
+        print(f"{key}: {value}")
+
+    await jp.disconnect()
